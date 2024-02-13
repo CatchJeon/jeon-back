cd5d7e4c0ff03616ed341d3337bbab091deb6790
subin <axhtl@naver.com>
2024-02-13 17:19:26 +0900

    feature : mail, nickname form validation

--- src/main/java/com/example/catchjeon_back/controller/RegisterController.java
+++ src/main/java/com/example/catchjeon_back/controller/RegisterController.java
@@ -3,9 +3,12 @@ package com.example.catchjeon_back.controller;
 import com.example.catchjeon_back.dto.RegisterForm;
 import com.example.catchjeon_back.entity.Register;
 import com.example.catchjeon_back.service.RegisterService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,24 @@ public class RegisterController {
     private RegisterService registerService;
 
     @PostMapping("/api/register")
-    public ResponseEntity<Register> create_register(@RequestBody RegisterForm dto){
+    public ResponseEntity<?> create_register(@Valid @RequestBody RegisterForm dto,
+                                             BindingResult bindingResult){
+        // 유효성 검사 오류가 발생한 경우
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println(field.getField() + ": " + message);
+
+                sb.append("field: " + field.getField());
+                sb.append("message: " + message);
+            });
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+        }
+
+        //유효성 검사 통과 후 처리(DB에 저장됨)
         Register created = registerService.create(dto);
         return (created != null) ?
                 ResponseEntity.status(HttpStatus.OK).body(created) :
--- src/main/java/com/example/catchjeon_back/dto/RegisterForm.java
+++ src/main/java/com/example/catchjeon_back/dto/RegisterForm.java
@@ -1,15 +1,25 @@
 package com.example.catchjeon_back.dto;
 
 import com.example.catchjeon_back.entity.Register;
-import lombok.AllArgsConstructor;
-import lombok.ToString;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @ToString
 public class RegisterForm {
     private Long id;
+
+    @Pattern(regexp="^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$")
     private String mail;
+
     private String passw;
+
+    @Size(max=10, message="닉네임 글자 수는 10자리를 넘을 수 없습니다.")
     private String nickname;
     public Register toEntity() {
         return new Register(id, mail, passw, nickname);
