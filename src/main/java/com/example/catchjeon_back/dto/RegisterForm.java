ea32e9cb42ff394c030784e8590b581259836806
subin <axhtl@naver.com>
2024-02-13 18:09:02 +0900

    feature : add mail and passw and nickname validation(NotBlank) in RegisterForm

--- src/main/java/com/example/catchjeon_back/dto/RegisterForm.java
+++ src/main/java/com/example/catchjeon_back/dto/RegisterForm.java
@@ -1,9 +1,7 @@
 package com.example.catchjeon_back.dto;
 
 import com.example.catchjeon_back.entity.Register;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -14,11 +12,14 @@ import lombok.*;
 public class RegisterForm {
     private Long id;
 
+    @NotBlank(message="null, 빈 문자열, 공백을 허용하지 않습니다.")
     @Pattern(regexp="^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$", message="이메일 형식으로 써주세요.")
     private String mail;
 
+    @NotBlank(message="null, 빈 문자열, 공백을 허용하지 않습니다.")
     private String passw;
 
+    @NotBlank(message="null, 빈 문자열, 공백을 허용하지 않습니다.")
     @Size(max=10, message="닉네임 글자 수는 10자리를 넘을 수 없습니다.")
     private String nickname;
     public Register toEntity() {
