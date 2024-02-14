4487aaa46c746c2cf7de1707a0115d34448cf08a
chaemin8600 <chaemin8600@gachon.ac.kr>
2024-02-14 16:33:55 +0900

    f

--- src/main/java/com/example/catchjeon_back/repository/RegisterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.catchjeon_back.repository;
-
-import com.example.catchjeon_back.entity.Register;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RegisterRepository extends CrudRepository<Register, Long> {
-
-}
